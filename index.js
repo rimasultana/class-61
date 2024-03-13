85578916690f4d1ad64becbef0e5b415ca6e34b5
rimasultana <rimasultana20225@gmail.com>
2024-03-13 19:43:13 +0600

    matches

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sentence = "Rima is a good gril. he is very honest.Rima fathers name Golam Mostafa. rima mothers name shemsunnaher."
 
-const matches = sentence.match(/Rima/gi);
+const matches = sentence.match(/Rimas/gi);
 
-const text = matches.length;
+const text = matches? matches.length: 0;
 
 console.log(text);
