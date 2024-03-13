1ecc5011a217ec3d45ae6312e3890f46cd098f7d
rimasultana <rimasultana20225@gmail.com>
2024-03-13 21:24:46 +0600

    boolean

--- index.js
+++ index.js
@@ -1,63 +1,11 @@
-// const x = "Learn with sumit. he is very honest. sumit is best of the world developer.";
 
-// const matches = x.match(/sumits/gi);
+const mixedArr = ["lws", undefined,
+"learn with sumi", false, "", "apple", 40, "k", true, "Thank you all", NaN];
 
-// const text = matches? matches.length: 0;
+const trueArray = mixedArr.filter(Boolean);
 
-// console.log(text);
-
-
-// let position = x.search(/sumits/i);
-
-// position = position>= position:"not found";
+console.log(trueArray);
 
 
-// console.log(position);
-
-// function linearSearch (arr, val) {
-//     const length = arr.length;
-// for (let i = 0; i < length; i++) {
-//     if (arr[i] === val) {
-//         return i;
-//     }
-// }
-// return "not found";
-// }
-
-// console.log(linearSearch(['a', 'b', 'c', 'd', 'c'],'c'));
-
-// function longestString (names) {
-//     let longestWord = '';
-
-// for (name of names) {
-//     if (name.length >longestWord.length) {
-//         longestWord = name;
-//     }
-// }
-// return [longestWord, names.indexOf(longestWord)];
-// }
-
-// console.log(longestString(['sumi saha', 'Learn with sumit', 'akash']));
 
 
-function fizzBuzz (number) {
-    for (let i = 1; i <=number; i++) {
-        if (i % ===0) {
-            if( i% 15 === 0) {
-                console.log(`${i} is fizzBuzz`);
-            } else(
-                i % 3 === 0
-            ) {
-                console.log(`${i} is fizz`);
-            } else(
-                i % 5 === 0
-            ) {
-                console.log(`${i} is Buzz`);
-            } else{
-                console.log(i);
-            }
-        }
-    }
-}
-
-fizzBuzz(100);
