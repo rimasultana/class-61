f195d6564edfbf91b4f4f34735aeec53f0e11c3b
rimasultana <rimasultana20225@gmail.com>
2024-03-13 20:01:26 +0600

    length

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const x = "Learn with sumit. he is very honest. sumit is best of the world developer.";
 
-const matches = x.match(/sumit/gi);
+const matches = x.match(/sumits/gi);
 
-const text = matches.length;
+const text = matches? matches.length: 0;
 
 console.log(text);
+
